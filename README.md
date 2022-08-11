309a6cd1df6dfd2cabbfabc0b91136627423a192
soorajabraham1 <soorajabraham1@gmail.com>
2022-08-11 17:09:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Text2Speech_NLP
-This is a project in which we can search in google based on voice input.
-
-
-In this project i have given only one website name. Initially if I spell the word wikipedia, then it enters to a loop which I have programmed only to proceed if the word wikipedia was recognized. the any word that is to be searched inside the wikipedia can be spelt .
-
+This is a project in which we can search in google based on voice input
--- speech2text.py
+++ speech2text.py
@@ -1,30 +1,28 @@
 import speech_recognition as sr
 import webbrowser as wb
 
-r1=sr.Recognizer()
-
 r2=sr.Recognizer()
 r3=sr.Recognizer()
 
 
 with sr.Microphone(device_index=0) as source:
-    print('[search Sooraj: search youtube]')
     print('speak now')
     audio=r3.listen(source)
-    print(r2.recognize_google(audio))
-if 'Wikipedia' in r2.recognize_google(audio):
-    r2= sr.Recognizer()
-    url='https://en.wikipedia.org/wiki/'
-    
-    with sr.Microphone() as source:
-        print('search')
-        audio=r2.listen(source)
-        try:
-            get= r2.recognize_google(audio)
-            print(get)
-            wb.get().open_new(url+get)
+    engine=r2.recognize_google(audio)
+    print(engine)
 
-        except sr.UnknownValueError:
-            print('error')
-        except sr.RequestError as e:
-            print('failed'.format(e))
+url1='https://www.'
+url2='.com/search?q='
+    
+with sr.Microphone() as source:
+    print('search')
+    audio=r2.listen(source)
+    try:
+        get= r2.recognize_google(audio)
+        print(get)
+        wb.get().open_new(url1+engine+url2+get)
+
+    except sr.UnknownValueError:
+        print('error')
+    except sr.RequestError as e:
+        print('failed'.format(e))
